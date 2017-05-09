99e882c2bf455a8378a2e36060f58439d5cd4869
Christian <brenninc@cs.man.ac.uk>
2017-05-09 10:01:06 +0100

    Added my automatic make file

---
+++ automatic_make.sh
@@ -0,0 +1,39 @@
+# This script assumes it is run from the directory holding all github projects in parellel
+# sh SupportScripts/automatic_make.sh
+
+cd spinnaker_tools
+source $PWD/setup
+make clean
+make || exit $?
+cd ..
+cd spinn_common
+make clean
+make || exit $?
+make install
+cd ..
+cd SpiNNMan/c_models/reinjector/
+make clean
+make || exit $?
+cd ../../..
+cd SpiNNFrontEndCommon/c_common/
+cd front_end_common_lib/
+make install-clean
+cd ..
+make clean
+make || exit $?
+make install
+cd ../..
+cd sPyNNaker/neural_modelling/
+make clean
+make || exit $?
+source $PWD/setup
+cd ../../sPyNNakerExternalDevicesPlugin/neural_modelling/
+make clean
+make || exit $?
+cd ../../SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
+make clean
+make || exit $?
+cd ../../../sPyNNakerExtraModelsPlugin/neural_modelling
+make clean
+make || exit $?
+echo "completed"
