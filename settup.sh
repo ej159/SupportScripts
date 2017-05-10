b97788c4176eabf6289e93ab4afe4fbf51f78697
Christian <brenninc@cs.man.ac.uk>
2017-05-10 13:03:08 +0100

    user python setup

---
+++ settup.sh
@@ -0,0 +1,22 @@
+# This script assumes it is run from the directory holding all github projects in parellel
+# sh SupportScripts/settip.sh a_branch_name
+
+do_setup(){
+	cd $1 || exit $?
+	python setup.py develop --no-deps --user || exit $1
+    cd ..
+}
+
+do_setup SpiNNUtils
+do_setup SpiNNMachine
+do_setup SpiNNStorageHandlers
+do_setup SpiNNMan
+do_setup PACMAN
+do_setup DataSpecification
+do_setup SpiNNFrontEndCommon
+do_setup sPyNNaker
+do_setup sPyNNaker7
+do_setup sPyNNaker8
+do_setup sPyNNakerExternalDevicesPlugin
+do_setup sPyNNaker7ExternalDevicesPlugin
+do_setup sPyNNaker8ExternalDevicesPlugin
