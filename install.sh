d2b5f02070d67bb0479ed6156c7da9ed67cc2391
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-05-25 10:01:31 +0100

    Script to do git installs as required.

---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+
+check_or_install(){
+    if [ -d "$1" ]; then
+        # Control will enter here if DIRECTORY exists.
+        if [ -d "$1/.git" ]; then
+            echo $1 already exists
+        else
+            echo Directory $1 exists but does not appear to be connected to git
+            echo Unable to install $2 there
+            exit
+        fi
+    else
+        while true; do
+            read -p "Do you wish to install $2 to $(pwd)/$1" yn
+            case $yn in
+                [Yy]* ) echo instaling $2;
+                    git clone $2
+                    echo installed $2 to $(pwd)/$1;
+                    break;;
+                [Nn]* ) echo Exiting script please rerun or install manually
+                    exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
+}
+
+
+case $1 in
+    *7 ) echo "Installing for PyNN7";
+        break;;
+    *8 ) echo "Installing for PyNN8";
+        break;;
+    [Bb]* ) echo "Installing both for PyNN7 and PyNN8";
+        break;;
+    * ) echo "Please specifiy if you wish to install for PyNN7, PyNN8, or Both.";
+        exit;;
+esac
+check_or_install spinnaker_tools https://github.com/SpiNNakerManchester/spinnaker_tools.git
+check_or_install spinn_common https://github.com/SpiNNakerManchester/spinn_common.git
+check_or_install SpiNNUtils https://github.com/SpiNNakerManchester/SpiNNUtils.git
+check_or_install SpiNNMachine https://github.com/SpiNNakerManchester/SpiNNMachine.git
+check_or_install SpiNNStorageHandlers https://github.com/SpiNNakerManchester/SpiNNStorageHandlers.git
+check_or_install PACMAN https://github.com/SpiNNakerManchester/PACMAN.git
+check_or_install SpiNNMan https://github.com/SpiNNakerManchester/SpiNNMan.git
+check_or_install DataSpecification https://github.com/SpiNNakerManchester/DataSpecification.git
+check_or_install SpiNNFrontEndCommon https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon.git
+check_or_install SpiNNakerGraphFrontEnd https://github.com/SpiNNakerManchester/SpiNNakerGraphFrontEnd.git
+check_or_install sPyNNaker https://github.com/SpiNNakerManchester/sPyNNaker.git
+check_or_install sPyNNakerExternalDevicesPlugin https://github.com/SpiNNakerManchester/sPyNNakerExternalDevicesPlugin.git
+check_or_install sPyNNakerExtraModelsPlugin https://github.com/SpiNNakerManchester/sPyNNakerExtraModelsPlugin.git
+check_or_install IntroLab https://github.com/SpiNNakerManchester/IntroLab.git
+
+
+case $1 in
+    *7 ) echo "Seven";
+        check_or_install sPyNNaker7 https://github.com/SpiNNakerManchester/sPyNNaker7.git ;
+        check_or_install sPyNNaker7ExternalDevicesPlugin https://github.com/SpiNNakerManchester/sPyNNaker7ExternalDevicesPlugin.git
+        check_or_install sPyNNaker7ExtraModelsPlugin https://github.com/SpiNNakerManchester/sPyNNaker7ExtraModelsPlugin.git
+        check_or_install sPyNNaker7NewModelTemplate https://github.com/SpiNNakerManchester/sPyNNaker7NewModelTemplate.git
+        check_or_install PyNN7Examples https://github.com/SpiNNakerManchester/PyNN7Examples.git
+        echo "Please insure your locally install PyNN is version 7"
+        break;;
+    *8 ) echo "Eight";
+        check_or_install sPyNNaker8 https://github.com/SpiNNakerManchester/sPyNNaker8.git ;
+        check_or_install sPyNNaker8ExternalDevicesPlugin https://github.com/SpiNNakerManchester/sPyNNaker8ExternalDevicesPlugin.git
+        check_or_install sPyNNaker8ExtraModelsPlugin https://github.com/SpiNNakerManchester/sPyNNaker8ExtraModelsPlugin.git
+        check_or_install sPyNNaker8NewModelTemplate https://github.com/SpiNNakerManchester/sPyNNaker8NewModelTemplate.git
+        check_or_install PyNN8Examples https://github.com/SpiNNakerManchester/PyNN8Examples.git
+        echo "Please insure your locally install PyNN is version 8"
+        break ;;
+    [Bb]* ) echo "Both";
+        check_or_install sPyNNaker7 https://github.com/SpiNNakerManchester/sPyNNaker7.git ;
+        check_or_install sPyNNaker7ExternalDevicesPlugin https://github.com/SpiNNakerManchester/sPyNNaker7ExternalDevicesPlugin.git
+        check_or_install sPyNNaker7ExtraModelsPlugin https://github.com/SpiNNakerManchester/sPyNNaker7ExtraModelsPlugin.git
+        check_or_install sPyNNaker7NewModelTemplate https://github.com/SpiNNakerManchester/sPyNNaker7NewModelTemplate.git
+        check_or_install PyNN7Examples https://github.com/SpiNNakerManchester/PyNN7Examples.git
+        check_or_install sPyNNaker8 https://github.com/SpiNNakerManchester/sPyNNaker8.git ;
+        check_or_install sPyNNaker8ExternalDevicesPlugin https://github.com/SpiNNakerManchester/sPyNNaker8ExternalDevicesPlugin.git
+        check_or_install sPyNNaker8ExtraModelsPlugin https://github.com/SpiNNakerManchester/sPyNNaker8ExtraModelsPlugin.git
+        check_or_install sPyNNaker8NewModelTemplate https://github.com/SpiNNakerManchester/sPyNNaker8NewModelTemplate.git
+        check_or_install PyNN8Examples https://github.com/SpiNNakerManchester/PyNN8Examples.git
+        echo "Warning you will need to use virtual machines or reinstall PyNN each time you switch Pynn version"
+        break;;
+    * ) echo "Please specifiy if you wish to install for PyNN7, PyNN8, or Both.";
+        exit;;
+esac
+
