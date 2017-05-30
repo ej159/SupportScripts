abfdf78634e0fb8624d0b19cd9d22303799b5de8
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-05-30 14:25:25 +0100

    Add another profile for use when dealing with code that include ASM

---
+++ vera++/profiles/spinnaker-asm.tcl
@@ -0,0 +1,22 @@
+set rules {
+    F001
+    F002
+
+    L001
+    L003
+    L004
+    L005
+    L006
+
+    T001
+    T003
+    T005
+    T006
+    T008
+    T009
+    T010
+    T015
+    T019
+}
+# rule T002 currently disabled; too much noise.
+# rule T007 disabled; profile fails on some files with embedded assembly code
