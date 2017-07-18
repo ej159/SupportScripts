62e6ad835bdec0533739aedf564ef68863ee569c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-07-18 12:51:54 +0100

    Extended the maximum path length

--- vera++/scripts/rules/F002.tcl
+++ vera++/scripts/rules/F002.tcl
@@ -4,53 +4,53 @@
 set maxDirectoryDepth [getParameter "max-directory-depth" 8]
 set maxDirnameLength [getParameter "max-dirname-length" 31]
 set maxFilenameLength [getParameter "max-filename-length" 50]
-set maxPathLength [getParameter "max-path-length" 100]
+set maxPathLength [getParameter "max-path-length" 200]
 
 foreach fileName [getSourceFileNames] {
     if {[string length $fileName] > $maxPathLength} {
-	report $fileName 1 "path name too long"
+    report $fileName 1 "path name too long"
     }
 
     set dirDepth 0
     foreach dir [file split [file dirname $fileName]] {
-	if {$dir == "/" || $dir == "." || $dir == ".."} {
-	    continue
-	}
+    if {$dir == "/" || $dir == "." || $dir == ".."} {
+        continue
+    }
 
-	incr dirDepth
+    incr dirDepth
 
-	if {[string length $dir] > $maxDirnameLength} {
-	    report $fileName 1 "directory name component too long"
-	    break
-	}
+    if {[string length $dir] > $maxDirnameLength} {
+        report $fileName 1 "directory name component too long"
+        break
+    }
 
-	set first [string index $dir 0]
-	if {[string is alpha $first] == 0 && $first != "_"} {
-	    report $fileName 1 "directory name should start with alphabetic character or underscore"
-	    break
-	}
+    set first [string index $dir 0]
+    if {[string is alpha $first] == 0 && $first != "_"} {
+        report $fileName 1 "directory name should start with alphabetic character or underscore"
+        break
+    }
 
-	if {[string first "." $dir] != -1} {
-	    report $fileName 1 "directory name should not contain a dot"
-	    break
-	}
+    if {[string first "." $dir] != -1} {
+        report $fileName 1 "directory name should not contain a dot"
+        break
+    }
     }
 
     if {$dirDepth >= $maxDirectoryDepth} {
-	report $fileName 1 "directory structure too deep"
+    report $fileName 1 "directory structure too deep"
     }
 
     set leafName [file tail $fileName]
     if {[string length $leafName] > $maxFilenameLength} {
-	report $fileName 1 "file name too long"
+    report $fileName 1 "file name too long"
     }
 
     set first [string index $leafName 0]
     if {[string is alpha $first] == 0 && $first != "_"} {
-	report $fileName 1 "file name should start with alphabetic character or underscore"
+    report $fileName 1 "file name should start with alphabetic character or underscore"
     }
 
     if {[llength [split $leafName .]] > 2} {
-	report $fileName 1 "file name should not contain more than one dot"
+    report $fileName 1 "file name should not contain more than one dot"
     }
 }
