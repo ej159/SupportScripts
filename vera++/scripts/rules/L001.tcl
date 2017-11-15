6e8c191a0ded8261d31d4ce238e8280bf2a5ad84
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-11-15 11:12:59 +0000

    Better error message in case that has been seen a few times

--- vera++/scripts/rules/L001.tcl
+++ vera++/scripts/rules/L001.tcl
@@ -6,11 +6,20 @@ set strictMode [getParameter "strict-trailing-space" 0]
 foreach f [getSourceFileNames] {
     set lineNumber 1
     set previousIndent ""
-    foreach line [getAllLines $f] {
 
-	if [regexp {^.*\r$} $line] {
-	  report $f $lineNumber "CRLF line ending"
-	  set line [string range $line 0 end-1]
+    # Special hack: handle comments on the last line
+    if {[catch {getAllLines $f} lines]} {
+    	set fd [open $f]
+    	set lineNumber [llength [split [read $fd] "\n"]]
+    	close $fd
+    	report $f $lineNumber "file needs to end with newline"
+    	continue
+    }
+
+    foreach line $lines {
+	if {[regexp {^.*\r$} $line]} {
+	    report $f $lineNumber "CRLF line ending"
+	    set line [string range $line 0 end-1]
 	}
 	if [regexp {^.*[[:space:]]+$} $line] {
 	    if {$strictMode || [string trim $line] != "" || $line != $previousIndent} {
