ccc9c45aa9e70e36cb8b9e891526846944aa2f9c
rowleya <Andrew.Rowley@manchester.ac.uk>
2018-02-15 14:35:40 +0000

    Fixed issues in scripts

--- automatic_make.sh
+++ automatic_make.sh
@@ -6,13 +6,16 @@ do_make() {
         # Control will enter here if DIRECTORY exists.
         make -C $1 clean || exit $?
         make -C $1 || exit $?
-        if [ $# == "install" ]; then
+        if [ "$2" == "install" ]; then
             make -C $1 install || exit $?
         fi
     fi
-        
+}
+
+cd spinnaker_tools && source ./setup && cd ..
 do_make spinnaker_tools
 do_make spinn_common install
 do_make SpiNNFrontEndCommon/c_common/ install
+cd sPyNNaker/neural_modelling && source ./setup && cd ..
 do_make sPyNNaker/neural_modelling/
 do_make SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 # This script assumes it is run from the directory holding all github projects in parellel
 # sh SupportScripts/settip.sh a_branch_name
 
-do_setup(){
-	cd $1 || exit $?
-	python setup.py develop --no-deps --user || exit $1
-    cd ..
+do_setup() {
+    if [ -d $1 ]; then
+        cd $1 
+        if [ -z "$VIRTUAL_ENV" ]; then
+            python setup.py develop --user || exit $1
+        else
+            python setup.py develop || exit $1
+        fi
+        cd ..
+        echo "Finished setup of $1"
+    fi
 }
 
 do_setup SpiNNUtils
@@ -13,6 +20,7 @@ do_setup SpiNNStorageHandlers
 do_setup SpiNNMan
 do_setup PACMAN
 do_setup DataSpecification
+do_setup spalloc
 do_setup SpiNNFrontEndCommon
 do_setup SpiNNakerGraphFrontEnd
 do_setup sPyNNaker
