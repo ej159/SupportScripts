4fea54c04534877d064308d1485f16d7427703d7
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-20 10:08:26 +0100

    better make file

---
+++ automatic-spinnaker.mk
@@ -0,0 +1,22 @@
+REQUIRED_REPOS = spinnaker_tools spinn_common SpiNNFrontEndCommon \
+	sPyNNaker sPyNNaker8NewModelTemplate #SpiNNakerGraphFrontEnd
+
+all: $(REQUIRED_REPOS)
+	$(MAKE) -C spinnaker_tools clean all
+	$(MAKE) -C spinn_common clean all install
+	$(MAKE) -C SpiNNFrontEndCommon/c_common/front_end_common_lib install-clean
+	$(MAKE) -C SpiNNFrontEndCommon/c_common clean all install
+	$(MAKE) -C sPyNNaker/neural_modelling clean all
+	#$(MAKE) -C SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples clean all
+	$(MAKE) -C sPyNNaker8NewModelTemplate/c_models clean all
+
+clean: $(REQUIRED_REPOS)
+	$(MAKE) -C spinnaker_tools clean
+	$(MAKE) -C spinn_common clean
+	$(MAKE) -C SpiNNFrontEndCommon/c_common clean
+	$(MAKE) -C sPyNNaker/neural_modelling clean
+	#$(MAKE) -C SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples clean
+	$(MAKE) -C sPyNNaker8NewModelTemplate/c_models clean all
+
+.PHONY: all clean
+
