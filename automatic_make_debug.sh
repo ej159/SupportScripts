100cb61aa70e017f925283f0797504df6e5f966a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-08-08 17:11:11 +0100

    Make additional changes

--- automatic_make_debug.sh
+++ automatic_make_debug.sh
@@ -1,30 +1,34 @@
 # This script assumes it is run from the directory holding all github projects in parellel
 # sh SupportScripts/automatic_make.sh
 
-cd spinnaker_tools
-source $PWD/setup
-make clean
-make || exit $?
-cd ..
-cd spinn_common
-make clean
-make install-clean
-make || exit $?
-make install
-cd ..
-cd SpiNNFrontEndCommon/c_common/
-cd front_end_common_lib/
-make install-clean
-cd ..
-make clean
-make FEC_DEBUG=DEBUG || exit $?
-make install
-cd ../..
-cd sPyNNaker/neural_modelling/
-source $PWD/setup.bash
-make clean
-make SPYNNAKER_DEBUG=DEBUG || exit $?
-cd ../../SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
-make clean
-make || exit $?
-echo "completed"
+do_make() {
+    if [ -d "$1" ]; then
+        # Control will enter here if DIRECTORY exists.
+        # Run setup.bash if it exists
+        if [ -f "$1/setup.bash" ]; then
+            cd $1
+            source setup.bash
+            cd -
+        fi
+        # Clean
+        make -C $1 clean || exit $?
+        # Clean installation; ignore error if install-clean doesn't work
+        # (probably because there is no install clean for most targets)
+        make -C $1 install-clean || true
+        # Make
+        make -C $1 $3 || exit $?
+        # Install if needed
+        if [ "$2" == "install" ]; then
+            make -C $1 install || exit $?
+        fi
+    fi
+}
+
+cd spinnaker_tools && source ./setup && cd ..
+do_make spinnaker_tools
+do_make spinn_common install
+do_make SpiNNFrontEndCommon/c_common/front_end_common_lib install FEC_DEBUG=DEBUG
+do_make SpiNNFrontEndCommon/c_common/ install
+cd sPyNNaker/neural_modelling && source ./setup.bash && cd ../..
+do_make sPyNNaker/neural_modelling/ noinstall SPYNNAKER_DEBUG=DEBUG
+do_make SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
