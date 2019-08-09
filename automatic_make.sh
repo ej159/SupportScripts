fedd6212ca8dd889c86ac2ede6211a54d228504e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-08-09 10:18:09 +0100

    Add new model template and fix debug to be similar to not debug

--- automatic_make.sh
+++ automatic_make.sh
@@ -30,9 +30,9 @@ do_make() {
 }
 
 do_make spinnaker_tools
-#do_make spinn_common install
-#do_make SpiNNFrontEndCommon/c_common/front_end_common_lib install
-#do_make SpiNNFrontEndCommon/c_common/ install
-#do_make sPyNNaker/neural_modelling/
-#do_make sPyNNaker8NewModelTemplate/c_models/
-#do_make SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
+do_make spinn_common install
+do_make SpiNNFrontEndCommon/c_common/front_end_common_lib install
+do_make SpiNNFrontEndCommon/c_common/ install
+do_make sPyNNaker/neural_modelling/
+do_make sPyNNaker8NewModelTemplate/c_models/
+do_make SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
