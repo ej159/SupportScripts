92a9c1c07ab9284e91b85f64af03d58ba59ef9ec
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-11-03 16:34:51 +0000

    remove refs

--- gitclone2.sh
+++ gitclone2.sh
@@ -2,18 +2,23 @@
 
 REPO=$1
 TARGET=$2
+TRAVIS_BRANCH=$(echo ENVIRON["GITHUB_REF"] | cut -d'/' -f 3)
+
+echo $TRAVIS_BRANCH
 
 Branch=$(git ls-remote $REPO | awk '
     BEGIN {
     	branch = "master"
-    	target = ENVIRON["GITHUB_REF"]
+    	target = "refs/heads/" $TRAVIS_BRANCH"
     }
     $2==target {
-    	branch = ENVIRON["GITHUB_REF"]
+    	branch = $TRAVIS_BRANCH
     }
     END {
     	print branch
     }')
 
+echo $Branch
+echo $TARGET
 git clone --branch $Branch $REPO $TARGET || exit $?
 echo "checked out branch $Branch of $REPO"
