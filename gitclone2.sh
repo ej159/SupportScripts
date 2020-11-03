69339dbc8149655f389abd2d93f5e488c95a85dd
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-11-03 15:40:00 +0000

    ref head is in the github ref

--- gitclone2.sh
+++ gitclone2.sh
@@ -6,7 +6,7 @@ TARGET=$2
 Branch=$(git ls-remote $REPO | awk '
     BEGIN {
     	branch = "master"
-    	target = "refs/heads/" ENVIRON["GITHUB_REF"]
+    	target = ENVIRON["GITHUB_REF"]
     }
     $2==target {
     	branch = ENVIRON["GITHUB_REF"]
