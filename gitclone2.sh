0008249088613f9b3b50bcf2a0810fe882abace3
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2020-11-04 07:25:38 +0000

    Ugly but appears to work

--- gitclone2.sh
+++ gitclone2.sh
@@ -2,23 +2,23 @@
 
 REPO=$1
 TARGET=$2
+
+echo "TRAVIS_BRANCH="$TRAVIS_BRANCH
+echo "GITHUB_REF"=$GITHUB_REF
 TRAVIS_BRANCH=$(echo $GITHUB_REF | cut -d'/' -f 3)
-
 echo $TRAVIS_BRANCH
-
+export TRAVIS_BRANCH
 Branch=$(git ls-remote $REPO | awk '
     BEGIN {
     	branch = "master"
-    	target = "refs/heads/" $TRAVIS_BRANCH
+    	target = ENVIRON["GITHUB_REF"]
     }
     $2==target {
-    	branch = $TRAVIS_BRANCH
+    	branch = ENVIRON["TRAVIS_BRANCH"]
     }
     END {
     	print branch
     }')
 
-echo $Branch
-echo $TARGET
 git clone --branch $Branch $REPO $TARGET || exit $?
 echo "checked out branch $Branch of $REPO"
