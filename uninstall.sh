f3d9654e55accd780b5787a1a648b315c0bb51d1
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-11-24 14:23:40 +0000

    Do all the things needed to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,14 +2,23 @@
 # bash SupportScripts/uninstall.sh
 
 do_uninstall() {
-    (pip uninstall -y $1 && echo "Uninstalled $1") || echo "$1 is not installed"
+    if [ -d $1 ]; then
+        cd $1
+        if [ -z "$VIRTUAL_ENV" ] && [ -z "$CONDA_PREFIX" ] && [ -z "$ASROOT" ]; then
+            python setup.py develop --uninstall || pip uninstall -y $2 || exit $1
+        else
+            python setup.py develop --uninstall --user || pip uninstall --user -y $2 || exit $1
+        fi
+        cd ..
+        echo "Uninstalled $1"
+    fi
 }
 
 do_uninstall SpiNNUtils
 do_uninstall SpiNNMachine
 do_uninstall SpiNNMan
 do_uninstall SpiNNaker_PACMAN
-do_uninstall DataSpecification
+do_uninstall SpiNNaker_DataSpecification
 do_uninstall spalloc
 do_uninstall SpiNNFrontEndCommon
 do_uninstall SpiNNakerGraphFrontEnd
