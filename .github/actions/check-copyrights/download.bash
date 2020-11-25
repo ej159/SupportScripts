5a6d78767e9daf627d4fb5397e97a5a1efa5a0af
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-11-25 14:40:56 +0000

    Added efficiency check and branding

--- .github/actions/check-copyrights/download.bash
+++ .github/actions/check-copyrights/download.bash
@@ -8,6 +8,11 @@ ratver=$RAT_VERSION
 apachebase=$RAT_MIRROR
 raturl="${apachebase}creadur/apache-rat-${ratver}/apache-rat-${ratver}-bin.tar.gz"
 
+if [ -d "$SUPPORT_DIR/apache-rat-$RAT_VERSION" ]; then
+	echo "::debug::Already downloaded; skipping..."
+	exit 0
+fi
+
 if curl -s -I -D - "$raturl" 2>/dev/null | grep -q "application/x-gzip"; then
 	curl -s --output - "$raturl" | (cd "$SUPPORT_DIR" && tar -zxf -)
 	exit $?
