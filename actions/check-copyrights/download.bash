ac0a40043bf5dfd3b3ef59e00d7a42b639a9c131
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-02-03 15:33:54 +0000

    Add redirect

--- actions/check-copyrights/download.bash
+++ actions/check-copyrights/download.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$SUPPORT_DIR" -o ! -d "$SUPPORT_DIR" ]; then
-	echo "::error::Undetermined support directory! ($SUPPORT_DIR)"
-	exit 1
+  echo "::error::Undetermined support directory! ($SUPPORT_DIR)"
+  exit 1
 fi
 cd "$SUPPORT_DIR" || exit 2
 
@@ -10,13 +10,13 @@ dir=apache-rat-${RAT_VERSION}
 raturl="${RAT_MIRROR}creadur/$dir/$dir-bin.tar.gz"
 
 if [ -d $dir ]; then
-	echo "::debug::Already downloaded; skipping..."
-	exit 0
+  echo "::debug::Already downloaded; skipping..."
+  exit 0
 fi
 
-curl -s -I -D - "$raturl" 2>/dev/null | grep -q "application/x-gzip" || {
-	echo "::error::Version of RAT ($RAT_VERSION) is wrong or mirror is down (URL: $raturl)"
-	exit 1
+curl -s -I -L -D - "$raturl" 2>/dev/null | grep -q "application/x-gzip" || {
+  echo "::error::Version of RAT ($RAT_VERSION) is wrong or mirror is down (URL: $raturl)"
+  exit 1
 }
 
 curl -s --output - "$raturl" | tar -zxf -
