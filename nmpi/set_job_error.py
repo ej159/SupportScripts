b313c59a07f7531272df1fb7246921441b50c5d5
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-04-12 13:08:58 +0100

    Add some NMPI support scripts to get and update jobs

---
+++ nmpi/set_job_error.py
@@ -0,0 +1,29 @@
+"""
+Set a job from the queue to "Error" status.
+
+Authentication parameters can be found in the config files on the server.
+"""
+import argparse
+import requests
+
+NMPI_URL = "https://nmpi.hbpneuromorphic.eu/api/v2/queue/{}"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("job", help="The Job ID")
+parser.add_argument("username", help="The username to authenticate with")
+parser.add_argument("token", help="The token to authenticate with")
+
+args = parser.parse_args()
+headers = {
+    "Authorization": "ApiKey {}:{}".format(args.username, args.token),
+    "Content-Type": "application/json"
+}
+
+job_url = NMPI_URL.format(args.job)
+response = requests.get(job_url, headers=headers)
+job = response.json()
+print(job)
+
+job["status"] = "error"
+put_response = requests.put(job_url, json=job, headers=headers)
+print(put_response)
